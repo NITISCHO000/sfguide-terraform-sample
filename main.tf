c442b3690d7ad2236508b9987b32b6f2f6088376
Nitin <nitinsap005@gmail.com>
2021-11-03 05:51:38 +0530

    Add Database and Warehouse

--- main.tf
+++ main.tf
@@ -50,3 +50,29 @@ resource "snowflake_warehouse" "warehouse" {
         roles             = [snowflake_role.role.name]
         with_grant_option = false
     }
+
+resource "snowflake_schema" "schema" {
+        provider   = snowflake.sys_admin
+        database   = snowflake_database.db.name
+        name       = "TF_DEMO"
+        is_managed = false
+    }
+
+
+    resource "snowflake_schema_grant" "grant" {
+        provider          = snowflake.security_admin
+        database_name     = snowflake_database.db.name
+        schema_name       = snowflake_schema.schema.name
+        privilege         = "USAGE"
+        roles             = [snowflake_role.role.name]
+        with_grant_option = false
+    }
+
+ resource "snowflake_warehouse_grant" "grant" {
+        provider          = snowflake.security_admin
+        warehouse_name    = snowflake_warehouse.warehouse.name
+        privilege         = "USAGE"
+        roles             = [snowflake_role.role.name]
+        with_grant_option = false
+    }
+
